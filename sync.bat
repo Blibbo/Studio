9ae8d402b7c73dad2f687e5694da8d21f2b2e3a8
Simone Bonfante <52862495+Blibbo@users.noreply.github.com>
2024-02-09 00:38:02 +0100

    commit from laptop

--- sync.bat
+++ sync.bat
@@ -1,2 +1,2 @@
 cd "C:\Users\simon\Desktop\Studio"
-git pull && git add * && git commit -a -m "commit from desktop" && git push
+git pull && git add * && git commit -a -m "commit from laptop" && git push
